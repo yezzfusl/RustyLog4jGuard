c0af13792821e50f632192ae3bd1de1128757b8e
YEZZFUSL <18398621+yezzfusl@users.noreply.github.com>
2024-08-14 21:36:42 -0400

    Optimize, Add Error Handling, and Refine.

--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,11 @@
 pub struct Config {
     pub path: String,
     pub format: String,
+    pub threads: Option<usize>,
 }
 
 impl Config {
-    pub fn new(path: String, format: String) -> Self {
-        Config { path, format }
+    pub fn new(path: String, format: String, threads: Option<usize>) -> Self {
+        Config { path, format, threads }
     }
 }
